638c557b016c75e36a25acf0095fcf7496dc1cb7
MArcinKotkiewic <markot72@o2.pl>
2021-12-18 09:56:17 +0100

    a

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # tu bedzie gra kólko i krzyżyk
 
-PlanszaDoGry = {j}hhh
+PlanszaDoGry = {'7':' ','8':' ','9':' ',
+                '4':' ','5':' ','6':' ',
+                '1':' ','2':' ','3':' '}
+                
