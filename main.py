25906faa0e77cd38a70c86b910c05d0a1b24f9d5
MArcinKotkiewic <markot72@o2.pl>
2021-12-18 10:30:36 +0100

    plansza do gry

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 # tu bedzie gra kólko i krzyżyk
+#%%
 
 PlanszaDoGry = {'7':' ','8':' ','9':' ',
                 '4':' ','5':' ','6':' ',
                 '1':' ','2':' ','3':' '}
-                
+
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+    print(klawiszeGry)
+
+def drukujPlansze():
+    print(' ' + '|' + ' ' + '|' + ' ')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|' + ' ')
+    print('-+-+-')
+    print(' ' + '|' + ' ' + '|' + ' ')
+drukujPlansze()
+
+# %%
+
+PlanszaDoGry = {'7':' ','8':' ','9':' ',
+                '4':' ','5':' ','6':' ',
+                '1':' ','2':' ','3':' '}
+
+klawiszeGry=[]
+
+for key in PlanszaDoGry:
+    klawiszeGry.append(key)
+
+
+def drukujPlansze(pole):
+    print(f"{pole['7']}|{pole['8']}|{pole['9']}")
+    print('-+-+-')
+    print(f"{pole['4']}|{pole['5']}|{pole['6']}")
+    print('-+-+-')
+    print(f"{pole['1']}|{pole['2']}|{pole['3']}")
+
+drukujPlansze(PlanszaDoGry)
+# %%
