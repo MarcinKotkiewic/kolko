59ffb96ec277eb26be17889da301bbf914860d0c
MArcinKotkiewic <markot72@o2.pl>
2021-12-18 14:18:52 +0100

    drugi zapis

--- pierwszeKlasy/pierwszeKlasy.py
+++ pierwszeKlasy/pierwszeKlasy.py
@@ -1,4 +1,5 @@
 # #%%
+from kot import Kot
 # class MojaKlasa:
 #     def wyswietl(x):
 #         return 'Witaj Świecie'
@@ -29,7 +30,11 @@ wtc.podstawa_b=200
 wtc.wysokosc_h=400
 print(wtc.Objetosc())
 
-
+Garfild=Kot()
+Garfild.imie ='Garfild'
+Garfild.wiek = 20
+Garfild.waga=9
+Garfild.jedzenie()
 #%%
 
 class Prostopadloscian:
@@ -63,10 +68,12 @@ szopa1 = szopa(2,3,4)
 
 # %%
 class Szopa:
+    pomalowaneBudynki = 0
     def __init__(self, bok_a, bok_b, wys_h):
         self.podstawa_a = bok_a
         self.podstawa_b = bok_b
         self.wysokosc_h = wys_h
+        Szopa.pomalowaneBudynki += 1
 
     def Pomaluj(self):
         return 2*(self.podstawa_a + self.podstawa_b) * self.wysokosc_h
@@ -75,7 +82,7 @@ class Szopa:
 szopa1 = Szopa(2, 3, 5)
 print(szopa1.Pomaluj())
 szopa2 = Szopa(5, 1, 2)
-print(szopa2.Pomaluj())
+print(szopa1.Pomaluj())
 # %%
 
 class koty:
@@ -87,5 +94,38 @@ class koty:
         self.wiek = 0
         self.waga = 0
     def 
+
+    
+#%%
+class Kot:
+    pomalowaneBudynki = 0
+    def __init__(self,):
+        self.name = ''
+        self.kolor_oczu = ''
+        self.kolor_sersci = ''
+        self.dlugosc = 0
+        self.wysokosc = 0
+        self.wiek = 0
+        self.waga = 0
+
+    def mialczenie():
+        print("Miał")
+    
+    def spanie(self):
+        if self.wiek<=10  and self.wiek >0:
+            print('śpi godzinę')
+        elif self.wiek>10:
+            print('śpi 3 godziny')
+    
+    def jedzenie(self):
+        self.waga += 10
+        print('kot dobrze zjadł')
+
+    def drapanie(self):
+        if self.waga <=10:
+            print('straty małe')
+        else:
+            print('kot groźny dla otoczenia')
+
 
     
