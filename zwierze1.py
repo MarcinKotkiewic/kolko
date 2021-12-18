998d5f8a5e907eded2e764448f218d5cf306bc86
MArcinKotkiewic <markot72@o2.pl>
2021-12-18 15:20:18 +0100

    cos

---
+++ zwierze1.py
@@ -0,0 +1,47 @@
+
+#%%
+class Zwierze:
+    def __init__(self,nazwa,wiek,waga):
+        self.nazwa = nazwa
+        self.waga = waga
+        self.wiek=wiek
+    def podaj_dane(self):
+        print(f'Jestem zwierzciem {self.nazwa},mam {self.wiek} lat i ważę {self.waga} kg')
+class Slon(Zwierze):
+    pass
+
+class Lew(Zwierze):
+    def __init__(self):
+        self.iloscKlow=4
+
+class Papuga(Zwierze):
+    def __init__(self,nazwa,dlg_skrzydel):
+        self.iloscPior =4000
+        self.nazwa=nazwa
+        self.dlg_skrzydel = dlg_skrzydel
+
+class Hybryda(Lew,Papuga):
+    pass
+
+
+hyb=Hybryda()
+hyb.
+
+
+
+
+Dumbo = Slon('Słoń Dumbo',300,2)
+
+
+Simba=Lew()
+Simba.iloscKlow = 3
+Simba.wiek=34
+Simba.nazwa="Lew Simba"
+Simba.waga = 45
+
+Simba.podaj_dane()
+Dumbo.podaj_dane()
+
+
+
+# %%
